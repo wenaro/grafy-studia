a2f693542bcf01bf6ad2934088ae97145601adc6
marekhryszkiewicz <marek94zg@gmail.com>
2018-04-22 12:25:18 +0200

    przeszukiwanie w glab - DFS

--- graphs/src/main/java/edu/graphs/controller/GenerateController.java
+++ graphs/src/main/java/edu/graphs/controller/GenerateController.java
@@ -4,10 +4,7 @@ import edu.graphs.input.Form;
 import edu.graphs.input.Input;
 import edu.graphs.input.Type;
 import edu.graphs.model.Graph;
-import edu.graphs.service.CyclesService;
-import edu.graphs.service.EdgeService;
-import edu.graphs.service.GraphService;
-import edu.graphs.service.VertexService;
+import edu.graphs.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +23,15 @@ public class GenerateController {
     private final EdgeService edgeService;
 
     private final CyclesService cyclesService;
+    private final GraphSearchingService graphSearchingService;
 
     public GenerateController(final GraphService graphService, final VertexService vertexService,
-                              final EdgeService edgeService, final CyclesService cyclesService) {
+                              final EdgeService edgeService, final CyclesService cyclesService, final GraphSearchingService graphSearchingService) {
         this.graphService = graphService;
         this.vertexService = vertexService;
         this.edgeService = edgeService;
         this.cyclesService = cyclesService;
+        this.graphSearchingService = graphSearchingService;
     }
 
     private Graph graph = new Graph();
@@ -41,7 +40,7 @@ public class GenerateController {
 
     @GetMapping("/graph")
     public String generate(final Model model) {
-        addModelAttributes(model, new Input(), new Form(), null, null);
+        addModelAttributes(model, new Input(), new Form(), null, null, null);
         return "generate";
     }
 
@@ -51,8 +50,9 @@ public class GenerateController {
         this.input = input;
         graph = graphService.createGraph(input.getType(), input.getText(), input);
 
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
         cyclesService.znajdzKrytycznaKrawedz(graph);
+
         return "generate";
     }
 
@@ -60,7 +60,7 @@ public class GenerateController {
             params = "action=clear")
     public String clearGraph(final Model model, @ModelAttribute final Input input) {
         graph = new Graph();
-        addModelAttributes(model, input, new Form(), null, null);
+        addModelAttributes(model, input, new Form(), null, null, graphSearchingService.DFS(graph));
         return "generate";
     }
 
@@ -68,7 +68,7 @@ public class GenerateController {
             params = "action=update")
     public String updateGraph(final Model model, @ModelAttribute final Input input) {
         graph = graphService.createGraph(Type.NEIGHBORHOOD_LIST, input.getConversion(), input);
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
 
         return "generate";
     }
@@ -77,7 +77,7 @@ public class GenerateController {
             params = "action=add")
     public String addVertex(final Model model, @ModelAttribute final Form form) {
         graph.addVertex(form.getVertex());
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
 
         return "generate";
     }
@@ -86,7 +86,7 @@ public class GenerateController {
             params = "action=update")
     public String updateVertex(final Model model, @ModelAttribute final Form form) {
         vertexService.updateVertex(graph, form);
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
 
         return "generate";
     }
@@ -95,7 +95,7 @@ public class GenerateController {
             params = "action=remove")
     public String removeVertex(final Model model, @ModelAttribute final Form form) {
         vertexService.removeVertex(graph, form);
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
         return "generate";
     }
 
@@ -103,7 +103,7 @@ public class GenerateController {
             params = "action=add")
     public String addEdge(final Model model, @ModelAttribute final Form form) {
         edgeService.addFormEdge(graph, form);
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
 
         return "generate";
     }
@@ -112,18 +112,19 @@ public class GenerateController {
             params = "action=remove")
     public String removeEdge(final Model model, @ModelAttribute final Form form) {
         edgeService.removeEdge(graph, form);
-        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()));
+        addModelAttributes(model, input, new Form(), cyclesService.checkIfEulerCycleExist(graph), cyclesService.checkIfHamiltonianCycleExist(graph.getVertices().size(), graph, graph.getVertices().get(0), new ArrayList<>(), new ArrayList<>()), graphSearchingService.DFS(graph));
 
         return "generate";
     }
 
-    private void addModelAttributes(final Model model, final Input input, final Form form, final String euler, final String hamilton) {
+    private void addModelAttributes(final Model model, final Input input, final Form form, final String euler, final String hamilton, final String DFS) {
         model.addAttribute("input", input);
         model.addAttribute("vertices", graph.getVertices());
         model.addAttribute("edges", graph.getEdges());
         model.addAttribute("form", form);
-        model.addAttribute("euler", euler);
-        model.addAttribute("hamilton", hamilton);
+        //  model.addAttribute("euler", euler);
+        //   model.addAttribute("hamilton", hamilton);
+        model.addAttribute("DFS", DFS);
     }
 
 }
--- graphs/src/main/java/edu/graphs/service/GraphSearchingService.java
+++ graphs/src/main/java/edu/graphs/service/GraphSearchingService.java
@@ -2,9 +2,12 @@ package edu.graphs.service;
 
 import edu.graphs.model.Edge;
 import edu.graphs.model.Graph;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class GraphSearchingService {
@@ -17,7 +20,63 @@ public class GraphSearchingService {
         return null;
     }
 
-    public void findBridges(){
+    public void findBridges() {
 
+    }
+
+
+    public String DFS(Graph graph) {
+        final List<String> stack = new ArrayList<>();
+        final List<String> vertices = graph.getVertices();
+        final List<String> visitedVertices = new ArrayList<>();
+        boolean isCohesion;
+
+        stack.add(vertices.get(0));
+
+        while (!stack.isEmpty()) {
+            final String v = stack.get(stack.size() - 1);
+            if (isVisited(v, visitedVertices)) {
+                break;
+            }
+            visitedVertices.add(v);
+            Set<String> sasiedzi = getNeighbors(graph, stack.get(stack.size() - 1));
+            stack.remove(stack.size() - 1);
+            for (String sasiad : sasiedzi) {
+                if (!visitedVertices.contains(sasiad)) {
+                    stack.add(sasiad);
+                }
+            }
+        }
+
+//        for (String vertex :
+//                visitedVertices) {
+//            System.out.println(vertex);
+//        }
+        return "Przeszukiwanie w głąb(DFS): " + String.join("->", visitedVertices);
+    }
+
+    private Set<String> getNeighbors(final Graph graph, String v) {
+        final Set<String> neighbors = new HashSet<>();
+        for (final Edge edge : graph.getEdges()) {
+            if (edge.getDestination().equals(v)) {
+                neighbors.add(edge.getSource());
+            }
+            if (edge.getSource().equals(v)) {
+                neighbors.add(edge.getDestination());
+            }
+        }
+
+        return neighbors;
+    }
+
+    private boolean isVisited(String v, List<String> visitedVertices) {
+        boolean isVisited = false;
+        for (final String vertex : visitedVertices) {
+            if (v.equals(vertex)) {
+                isVisited = true;
+            }
+        }
+
+        return isVisited;
     }
 }
